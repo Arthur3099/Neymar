cb75d605e63b16aeea226c4c6b7642fe6d745873
Arthur <arthur.gustavo.amaral@escola.pr.gov.br>
2024-09-05 04:12:18 +0000

    Artu 🐷🇲🇴

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
+function setup() {
+      createCanvas(777, 777);
+        background("#4AC34D");
+        }
 
+        function draw() {
+          stroke("#37773A");
+            fill("#F1F4F0");
+              
+                   if(mouseIsPressed) {
+                        rect(mouseX, mouseY, 20, 30);
+                          }
+}
